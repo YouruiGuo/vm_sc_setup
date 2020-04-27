c576f812284770ac4a1f8548c9e5aa6b81a8dda5
YouruiGuo <yourui@ualberta.ca>
2020-04-27 16:45:19 -0600

    macinit change

--- mac_init.sh
+++ mac_init.sh
@@ -24,10 +24,11 @@ vagrant ssh << EOF
 	jack_control start
 	cd /home/vagrant
 	#echo `aplay -L | grep CARD= -m1 | cut -d'=' -f 2`
-	echo -e '#!/bin/bash\njack_control ds alsa\njack_control dps device hw:\`aplay -L | grep CARD= -m1 | cut -d'=' -f 2\`\njack_control dps rate 48000\njack_control dps nperiods 2\njack_control dps period 4096\nsleep 2\njack_control start\n' >> /home/vagrant/start_jack.sh
+	echo -e '#!/bin/bash\njack_control stop\njack_control ds alsa\njack_control dps device hw:\`aplay -L | grep CARD= -m1 | cut -d'=' -f 2\`\njack_control dps rate 48000\njack_control dps nperiods 2\njack_control dps period 1024\nsleep 2\njack_control start\n' >> /home/vagrant/start_jack.sh
 	chmod u+x /home/vagrant/start_jack.sh
-	jack_control stop
-	echo '/home/vagrant/start_jack.sh' >> ~/.bashrc
+
+	#jack_control stop
+	#echo '/home/vagrant/start_jack.sh' >> ~/.bashrc
 	#cd /home/vagrant/.config/jack && cp /vagrant/conf.xml ./
 	#jack_control start
 EOF
